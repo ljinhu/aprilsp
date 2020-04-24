29aa4025d70f92901378d35faff1f54cc3c91d12
jinhu813 <jimbolix@163.com>
2020-04-24 17:02:34 +0800

    添加RabbitMq配置

---
+++ april-gateway-admin/src/main/java/com/jimbolix/april/gateway/admin/config/RabbitConfig.java
@@ -0,0 +1,12 @@
+package com.jimbolix.april.gateway.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: ruihui.li
+ * @Date: 2020/4/24 17:01
+ * @Description: 
+ */
+@Configuration
+public class RabbitConfig {
+}
