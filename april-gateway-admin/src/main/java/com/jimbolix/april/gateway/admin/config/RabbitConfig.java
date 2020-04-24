597c8111a9261b5ac5fc4af6cb5b296f8db4cd06
jinhu813 <2282402253@qq.com>
2020-04-24 17:11:53 +0800

    添加RabbitMq配置

--- april-gateway-admin/src/main/java/com/jimbolix/april/gateway/admin/config/RabbitConfig.java
+++ april-gateway-admin/src/main/java/com/jimbolix/april/gateway/admin/config/RabbitConfig.java
@@ -1,12 +1,26 @@
 package com.jimbolix.april.gateway.admin.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.support.converter.ContentTypeDelegatingMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @Author: ruihui.li
  * @Date: 2020/4/24 17:01
- * @Description: 
+ * @Description:
  */
 @Configuration
 public class RabbitConfig {
+
+    @Bean
+    MessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        return new ContentTypeDelegatingMessageConverter(new Jackson2JsonMessageConverter(objectMapper));
+    }
 }
